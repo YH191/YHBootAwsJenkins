123f0dfa8f0e982717ef48c0c4db60dbfd0a3ae5
ppyh09 <ppyh09@naver.com>
2022-06-12 06:35:49 +0900

    .

--- src/main/java/com/yh/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/yh/book/springboot/config/auth/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring().antMatchers( "/css/**", "/js/**", "/images/**",
-                "/favicon.ico", "/resources/**", "/error");
+                "/favicon.ico", "/resources/**", "/error", "/auth/**", "/modify", "/user/**");
     }
 
     @Override
