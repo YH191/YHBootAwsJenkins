aa146c7d436ddf42a8677ccde86e454bdee503a8
yhpark09 <ppyh9312@gmail.com>
2023-06-05 00:38:24 +0900

    add @EnableWebMvc annotation

--- src/main/java/com/yh/book/springboot/Application.java
+++ src/main/java/com/yh/book/springboot/Application.java
@@ -3,8 +3,10 @@ package com.yh.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /** 추후 war 빌드 및 배포시 SpringBootServletInitializer 상속*/
+@EnableWebMvc
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
