21c7477a4b1ecd3ad0b1ef2b954db428fad38ce9
yhpark09 <ppyh9312@gmail.com>
2023-06-04 15:54:41 +0900

    change annotation

--- src/test/java/com/yh/book/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/yh/book/springboot/web/ProfileControllerTest.java
@@ -4,7 +4,6 @@ package com.yh.book.springboot.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -14,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
     @LocalServerPort
     private int port;
--- src/test/java/com/yh/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/yh/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -6,7 +6,6 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -14,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
 public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
