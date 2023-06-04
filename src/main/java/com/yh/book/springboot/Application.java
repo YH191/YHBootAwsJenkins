a6cbb436b5563279e43129508dec24726e48d3bd
yhpark09 <ppyh9312@gmail.com>
2023-06-05 00:36:17 +0900

    del war settings /**/

--- src/main/java/com/yh/book/springboot/Application.java
+++ src/main/java/com/yh/book/springboot/Application.java
@@ -2,10 +2,11 @@ package com.yh.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /** 추후 war 빌드 및 배포시 SpringBootServletInitializer 상속*/
 @SpringBootApplication
-public class Application /*extends SpringBootServletInitializer*/ {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
