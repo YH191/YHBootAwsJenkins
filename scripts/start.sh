15cf9175c0616a666dc7e408832442360fa817c3
YH191 <ppyh09@naver.com>
2023-06-06 14:18:55 +0900

    edit naming

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=YHBootAwsJenkins
+PROJECT_NAME=YHBootAwsTravis
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.war $REPOSITORY/"
